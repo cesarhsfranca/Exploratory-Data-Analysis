b486d0b57df6cf07ffdf6f10bd149ce3bdb17b91
cesarhsfranca <cesarhsfranca@gmail.com>
2017-06-04 22:00:14 -0300

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,1 +1,14 @@
+## Read all the data:
+alldata <- read.csv("./Project1/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+alldata$Date <- as.Date(alldata$Date, format="%d/%m/%Y")
+
+## Subsetting the data for the period between 2007/02/01 and 2007/02/02
+data <- subset(alldata, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(alldata)
+
+## Convert the dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
 
